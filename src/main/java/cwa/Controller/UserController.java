e5a559668e65791f1999353bee5bec277d44b5fd
ChenWenao <747273894@qq.com>
2021-02-14 10:07:14 +0800

    补充内容

--- src/main/java/cwa/Controller/UserController.java
+++ src/main/java/cwa/Controller/UserController.java
@@ -1,13 +1,35 @@
 package cwa.Controller;
 
+import cwa.Bean.user;
 import cwa.Service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
+
+    //返回登陆页面
+    @GetMapping("/User/login")
+    public ModelAndView login() {
+        ModelAndView mav = new ModelAndView("login.html");
+        return mav;
+    }
+
+    @PostMapping("/User/register")
+    public String register(@ModelAttribute(value = "regUser")user regUser){
+        System.out.println(regUser.getUserName());
+        System.out.println(regUser.getPassword());
+        return "成功";
+    }
+
+
 
 
 }
